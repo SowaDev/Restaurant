2ccb81958d458cefc749211ec6a57f56c5bc36b7
SowaDev <piotrsowa@yahoo.com>
2021-10-25 18:43:10 +0200

    created controller for personal data

--- src/main/java/com/restaurant/controller/PersonalDataController.java
+++ src/main/java/com/restaurant/controller/PersonalDataController.java
@@ -1,2 +1,24 @@
-package com.restaurant.controller;public class PersonalDataController {
+package com.restaurant.controller;
+
+import com.restaurant.model.PersonalData;
+import com.restaurant.repositories.PersonalDataRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/personaldata")
+public class PersonalDataController {
+    private final PersonalDataRepository personalDataRepository;
+    public PersonalDataController(PersonalDataRepository personalDataRepository){
+        this.personalDataRepository = personalDataRepository;
+    }
+    @GetMapping()
+    public Iterable<PersonalData> getAllPersonalData(){
+        return this.personalDataRepository.findAll();
+    }
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED, reason = "Personal data was successfully added")
+    public PersonalData createNewPersonalData(@RequestBody PersonalData personalData) {
+        return this.personalDataRepository.save(personalData);
+    }
 }
