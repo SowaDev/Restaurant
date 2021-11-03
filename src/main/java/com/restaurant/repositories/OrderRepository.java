0df351105ee9bb18cc99fe98a35b1d39baaef139
SowaDev <piotrsowa@yahoo.com>
2021-11-03 10:27:18 +0100

    created repository for orders

--- src/main/java/com/restaurant/repositories/OrderRepository.java
+++ src/main/java/com/restaurant/repositories/OrderRepository.java
@@ -1,2 +1,14 @@
-package com.restaurant.repositories;public interface OrderRepository {
+package com.restaurant.repositories;
+
+import com.restaurant.model.Order;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+//    @Query("Select o FROM Order o WHERE " +
+//    "o.deliveryStatus = com.restaurant.enums.DeliveryStatus.")
+//    List<Order> checkInProgress
+
 }
