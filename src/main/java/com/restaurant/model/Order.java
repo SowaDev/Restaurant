73a0d3c6a0f0ed4927e4f1363b0c0e3d37035242
SowaDev <piotrsowa@yahoo.com>
2021-11-05 13:07:28 +0100

    Created nested builder class

--- src/main/java/com/restaurant/model/Order.java
+++ src/main/java/com/restaurant/model/Order.java
@@ -3,6 +3,7 @@ package com.restaurant.model;
 import com.restaurant.enums.DeliveryStatus;
 import lombok.*;
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -29,4 +30,55 @@ public class Order {
     private DeliveryStatus deliveryStatus;
     @Column(name = "PICK_UP")
     private Boolean isPickedUpByClient;
+    @Column(name = "DATE")
+    private Date date;
+
+    private Order(OrderBuilder orderBuilder){
+        this.orderList = orderBuilder.orderList;
+        this.address = orderBuilder.address;
+        this.personalData = orderBuilder.personalData;
+        this.comment = orderBuilder.comment;
+        this.totalPrice = orderBuilder.totalPrice;
+        this.deliveryStatus = orderBuilder.deliveryStatus;
+        this.isPickedUpByClient = orderBuilder.isPickedUpByClient;
+        this.date = orderBuilder.date;
+    }
+
+    public static class OrderBuilder{
+        //Required
+        private final List<Dish> orderList;
+        private final PersonalData personalData;
+        private final Double totalPrice;
+        private  final DeliveryStatus deliveryStatus;
+        private final Boolean isPickedUpByClient;
+        private final Date date;
+        //Optional
+        private Address address;
+        private String comment;
+
+        public OrderBuilder (List<Dish> orderList, PersonalData personalData, Double totalPrice,
+                             DeliveryStatus deliveryStatus, Boolean isPickedUpByClient, Date date){
+            this.orderList = orderList;
+            this.personalData = personalData;
+            this.totalPrice = totalPrice;
+            this.deliveryStatus = deliveryStatus;
+            this.isPickedUpByClient = isPickedUpByClient;
+            this.date = date;
+        }
+
+        public OrderBuilder withAddress(Address address){
+            this.address = address;
+            return this;
+        }
+
+        public OrderBuilder withComment(String comment){
+            this.comment = comment;
+            return this;
+        }
+
+        public Order build(){
+            return new Order(this);
+        }
+
+    }
 }
