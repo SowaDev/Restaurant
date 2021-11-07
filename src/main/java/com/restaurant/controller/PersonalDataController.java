877406f93b3c79471f9b0475b6f5b6ffabe4f575
SowaDev <piotrsowa@yahoo.com>
2021-11-07 11:10:49 +0100

    Added attribute's validation

--- src/main/java/com/restaurant/controller/PersonalDataController.java
+++ src/main/java/com/restaurant/controller/PersonalDataController.java
@@ -3,22 +3,32 @@ package com.restaurant.controller;
 import com.restaurant.model.PersonalData;
 import com.restaurant.repositories.PersonalDataRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/personaldata")
 public class PersonalDataController {
+
     private final PersonalDataRepository personalDataRepository;
+
     public PersonalDataController(PersonalDataRepository personalDataRepository){
         this.personalDataRepository = personalDataRepository;
     }
+
     @GetMapping()
     public Iterable<PersonalData> getAllPersonalData(){
         return this.personalDataRepository.findAll();
     }
+
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED, reason = "Personal data was successfully added")
-    public PersonalData createNewPersonalData(@RequestBody PersonalData personalData) {
+    public PersonalData createNewPersonalData(@RequestBody @Valid PersonalData personalData,
+                                              BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return personalData;
         return this.personalDataRepository.save(personalData);
     }
 }
--- src/main/java/com/restaurant/model/PersonalData.java
+++ src/main/java/com/restaurant/model/PersonalData.java
@@ -2,26 +2,33 @@ package com.restaurant.model;
 
 import lombok.*;
 import org.hibernate.Hibernate;
-
 import javax.persistence.*;
-import java.io.Serializable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Objects;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "PERSONAL_DATA")
-public class PersonalData implements Serializable{
+public class PersonalData{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    //@Column(name = "NAME")
+    @Column(name = "NAME")
+    @Pattern(regexp = "[A-Za-zżźćńółęąśŻŹĆŚŁÓ]+", message = "name is not valid")
     private String name;
-    //Column(name = "SURNAME")
+    @Column(name = "SURNAME")
+    @Pattern(regexp = "[A-Za-zżźćńółęąśŻŹĆŚŁÓ]+", message = "surname is not valid")
     private String surname;
-    //@Column(name = "EMAIL")
+    @Column(name = "EMAIL")
+    @Email
+    @NotBlank
     private String email;
-    //@Column(name = "PHONE_NUMBER")
+    @Column(name = "PHONE_NUMBER")
+    @Pattern(regexp = "\\d{9}",
+            message = "Phone number has to consist exactly 9 numbers")
     private String phoneNumber;
 
     @Override
