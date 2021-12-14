795c0ea395ba2538eb568f00e5f36442d5f95d91
SowaDev <piotrsowa@yahoo.com>
2021-12-14 23:12:09 +0100

    added find by category method

--- src/main/java/com/restaurant/repositories/DishRepository.java
+++ src/main/java/com/restaurant/repositories/DishRepository.java
@@ -1,9 +1,13 @@
 package com.restaurant.repositories;
 
+import com.restaurant.enums.DishCategory;
 import com.restaurant.model.Dish;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DishRepository extends CrudRepository<Dish, Integer> {
+    List<Dish> findByDishCategory(DishCategory dishCategory);
 }
