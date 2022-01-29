3c3d95f397f5d3060a081442dd5aaef2eadbcad3
SowaDev <piotrsowa@yahoo.com>
2022-01-29 12:45:48 +0100

    added address and personals to user: client, and created new user without address and personals

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO MENU VALUES (1, 'MAIN', 'Schabowy', 15.0);
 INSERT INTO MENU VALUES (2, 'SIDE', 'Frytki', 7.0);
+INSERT INTO address VALUES(1, '17', '234', 'Dabrowa', NULL, NULL, NULL, '32-231', 'Lenartowicza');
+INSERT INTO personals VALUES (1, 'email@email.com', 'Piotyr', '123123123', 'Sowyr');
 INSERT INTO User VALUES(1, '$2a$10$CpULeRv9LsofSMKhWT3sWuwZlrwk9hGByi/qo6ekNAoo0vhfiqjJ2', 'ADMIN', 'admin', NULL, NULL, NULL);
-INSERT INTO User VALUES(2, '$2a$10$KEIXP4g1sQmCAT8gE.dDO.4NqgrQ0zzzeDWglkGi0MaTZmd4abr9.', 'CLIENT', 'client', NULL, NULL, NULL);
+INSERT INTO User VALUES(2, '$2a$10$KEIXP4g1sQmCAT8gE.dDO.4NqgrQ0zzzeDWglkGi0MaTZmd4abr9.', 'CLIENT', 'client', 1, NULL, 1);
+INSERT INTO User VALUES(2, '$2a$10$KEIXP4g1sQmCAT8gE.dDO.4NqgrQ0zzzeDWglkGi0MaTZmd4abr9.', 'CLIENT', 'client2', 1, NULL, 1);
 INSERT INTO User VALUES(3, '$2a$10$jfKiXJdBmXKvN9yuM5e66.tPqZnZTN7xa5DhHYwdJ.jg4Oc90VoTm', 'EMPLOYEE', 'employee', NULL, NULL, NULL);
