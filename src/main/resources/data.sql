0d0557bc6d60eaee43fef63d02f1f9db0b292bfa
SowaDev <piotrsowa@yahoo.com>
2022-01-30 10:02:33 +0100

    fixed entries in initial data load

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO address VALUES(1, '17', '234', 'Dabrowa', NULL, NULL, NULL, '32-231'
 INSERT INTO personals VALUES (1, 'email@email.com', 'Piotyr', '123123123', 'Sowyr');
 INSERT INTO User VALUES(1, '$2a$10$CpULeRv9LsofSMKhWT3sWuwZlrwk9hGByi/qo6ekNAoo0vhfiqjJ2', 'ADMIN', 'admin', NULL, NULL, NULL);
 INSERT INTO User VALUES(2, '$2a$10$KEIXP4g1sQmCAT8gE.dDO.4NqgrQ0zzzeDWglkGi0MaTZmd4abr9.', 'CLIENT', 'client', 1, NULL, 1);
-INSERT INTO User VALUES(2, '$2a$10$KEIXP4g1sQmCAT8gE.dDO.4NqgrQ0zzzeDWglkGi0MaTZmd4abr9.', 'CLIENT', 'client2', 1, NULL, 1);
-INSERT INTO User VALUES(3, '$2a$10$jfKiXJdBmXKvN9yuM5e66.tPqZnZTN7xa5DhHYwdJ.jg4Oc90VoTm', 'EMPLOYEE', 'employee', NULL, NULL, NULL);
+INSERT INTO User VALUES(3, '$2a$10$KEIXP4g1sQmCAT8gE.dDO.4NqgrQ0zzzeDWglkGi0MaTZmd4abr9.', 'CLIENT', 'client2', NULL, NULL, NULL);
+INSERT INTO User VALUES(4, '$2a$10$jfKiXJdBmXKvN9yuM5e66.tPqZnZTN7xa5DhHYwdJ.jg4Oc90VoTm', 'EMPLOYEE', 'employee', NULL, NULL, NULL);
